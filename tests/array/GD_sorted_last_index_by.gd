543545bca279ddbdc556019f9ec81bcd113e8645
CasseyCola <project.cassey@gmail.com>
2024-01-15 21:17:42 +1300

    v0.0.6 Added sorted_index_by, sorted_index_of, sorted_last_index, sorted_last_index_by, sorted_last_index_of

---
+++ tests/array/GD_sorted_last_index_by.gd
@@ -0,0 +1,14 @@
+extends SimpleTest
+
+
+var array = [{"id":4}, {"id":5}, {"id":5}, {"id":5}, {"id":6}]
+func it_returns_the_index_of_last_occurence():
+	expect(GD_.sorted_last_index_by(array, {"id":5}, "id")).to.equal(4)
+
+
+func it_is_able_to_return_last_index_if_beyond_last():
+	expect(GD_.sorted_last_index_by(array, {"id":7}, "id")).to.equal(5)
+#
+#
+func it_is_able_to_return_last_index_if_below_first():
+	expect(GD_.sorted_last_index_by(array, {"id":1}, "id")).to.equal(0)
