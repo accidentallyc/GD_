dc71dbaf80449856495f0d1e2e7125da744b6448
Accidentallyc <project.cassey@gmail.com>
2024-03-05 20:10:57 +1300

    Converted __INTERNAL__ to static

--- addons/gd_/src/GD_base.gd
+++ addons/gd_/src/GD_base.gd
@@ -12,16 +12,9 @@ class UNDEFINED:
         
 static var static_self = self
 
-static var _UNDEF_ = Internal._UNDEF_:
-    get: return Internal._UNDEF_
+static var _UNDEF_ = __INTERNAL__._UNDEF_:
+    get: return __INTERNAL__._UNDEF_
 static var _EMPTY_ARRAY_ = []
-
-static var __INTERNAL__:Internal = Internal.new():
-    get:
-        # We gotta do this because static vars are null in EditorScripts 
-        if __INTERNAL__ == null:
-            __INTERNAL__ = Internal.new()
-        return __INTERNAL__
 
 """
 Reference to original functions
@@ -42,78 +35,89 @@ static func gd_warn(str:String):
 Internal utility function
 """
 
+static func _get_callable_result(callable:Callable):
+    return callable.call()
+
 ## Wrapping it in a class o we can hide the implementation from being visible
 ## when accessing GD_.
-class Internal:
+class __INTERNAL__:
     static var _UNDEF_ = UNDEFINED.new()
-    # Used for testing purposes
-    var last_warning
-    var is_warning_enabled = true
-    var string_to_path_rx:RegEx
-    var id_ctr = 0
     
-    var char_code_reference = "azAZ".to_utf8_buffer()
-    var key_a = char_code_reference[0]
-    var key_z = char_code_reference[1]
-    var key_A = char_code_reference[2]
-    var key_Z = char_code_reference[3]
+    """
+    Function useful for complex initializations without
+    exposing implementation
+    """
+    static func _get_callable_result(callable:Callable): 
+        return callable.call()
+        
+    # Used for testing purposes
+    static var last_warning
+    static var is_warning_enabled = true
+    static var id_ctr = 0
+    
+    static var char_code_reference = "azAZ".to_utf8_buffer()
+    static var key_a = char_code_reference[0]
+    static var key_z = char_code_reference[1]
+    static var key_A = char_code_reference[2]
+    static var key_Z = char_code_reference[3]
     
     """
     The subsection BELOW is dedicated to building the regex used in GD_.words
     """
-    var rx_words_postfixes = "|".join([
-        '(\'[dD])', 
-        '(\'[lL][lL])', 
-        '(\'[mM])', 
-        '(\'[rR][eE])', 
-        '(\'[sS])', 
-        '(\'[tT])', 
-        '(\'[vV][eE])',
-    ])
-    var rx_words_ordinals = "|".join([
-        '[sS][tT]', '[nN][dD]', '[rR][dD]', '[tT][hH]'
-    ])
-    var rx_words_args = {
-        "postfixes":rx_words_postfixes,
-        "ordinals": rx_words_ordinals
-    }
-    var rx_words = RegEx.create_from_string(
-        "|".join([
-            # Numbers
-            "([0-9]+({ordinals})?)".format(rx_words_args),
-            # Captilized words
-            "(\\p{Lu}{1}\\p{Ll}+({postfixes})?)".format(rx_words_args),
-            # All capped words,
-            "(\\p{Lu}+(?!\\p{Ll}))({postfixes})?".format(rx_words_args),
-            # lowercase everything
-            "(\\p{Ll}+)({postfixes})?".format(rx_words_args),
-            # Emojis and other chars
-            "(\\p{So})"
-        ])
+    static var rx_words = _get_callable_result(func ():
+            var rx_words_postfixes = "|".join([
+                '(\'[dD])', 
+                '(\'[lL][lL])', 
+                '(\'[mM])', 
+                '(\'[rR][eE])', 
+                '(\'[sS])', 
+                '(\'[tT])', 
+                '(\'[vV][eE])',
+            ])
+            var rx_words_ordinals = "|".join([
+                '[sS][tT]', '[nN][dD]', '[rR][dD]', '[tT][hH]'
+            ])
+            var rx_words_args = {
+                "postfixes":rx_words_postfixes,
+                "ordinals": rx_words_ordinals
+            }
+            return RegEx.create_from_string(
+                "|".join([
+                    # Numbers
+                    "([0-9]+({ordinals})?)".format(rx_words_args),
+                    # Captilized words
+                    "(\\p{Lu}{1}\\p{Ll}+({postfixes})?)".format(rx_words_args),
+                    # All capped words,
+                    "(\\p{Lu}+(?!\\p{Ll}))({postfixes})?".format(rx_words_args),
+                    # lowercase everything
+                    "(\\p{Ll}+)({postfixes})?".format(rx_words_args),
+                    # Emojis and other chars
+                    "(\\p{So})"
+                ])
+            )
     )
     """
     The subsection ABOVE is dedicated to building the regex used in GD_.words
     """
     
-    
-    # Keeps track of how many times
-    # a specific function has been called
-    # - Used in before and after trackers 
-    var callable_trackers = {}
-    
-    func _init():
-        string_to_path_rx = RegEx.new()
-        string_to_path_rx.compile(
-         str(
+    # Regex for converting string to path
+    static var string_to_path_rx:RegEx = RegEx.create_from_string(
+        str(
             "(\\[(?<key>[\\w\\s\'\"\\.]+)?\\])",
             "|",
             "(?<key>\'?[\\w\\s\\.]+\'?)",
             "|",
             "(?<key>\"?[\\w\\s\\.]+\"?)",
             )
-        )
+    )
     
-    func gd_warn(str:String):
+    
+    # Keeps track of how many times
+    # a specific function has been called
+    # - Used in before and after trackers 
+    static var callable_trackers = {}
+
+    static func gd_warn(str:String):
         last_warning = str
         if is_warning_enabled:
             printerr(str)
@@ -122,7 +126,7 @@ class Internal:
     ## This attempts to replicate the "variable arguments"
     ## Assumes that the user will fill from left to right.
     ## Will break on the first UNDEFINED
-    func to_clean_args(
+    static func to_clean_args(
         a = UNDEFINED.ref,
         b = UNDEFINED.ref,
         c = UNDEFINED.ref,
@@ -143,7 +147,7 @@ class Internal:
             
         return cleaned
         
-    func set_dict_deep(dict:Dictionary, paths:Array, value):
+    static func set_dict_deep(dict:Dictionary, paths:Array, value):
         var curr = dict
         var size = paths.size()
         for i in range(0,size):
@@ -156,18 +160,18 @@ class Internal:
                 curr[path] = {}
                 curr = curr[path]
                 
-    func base_is_array(thing, __UNUSED__ = null):
+    static func base_is_array(thing, __UNUSED__ = null):
         var type = typeof(thing)
         return type <= TYPE_PACKED_COLOR_ARRAY and type >= TYPE_ARRAY
 
-    func is_int_like(tmp):
+    static func is_int_like(tmp):
         return (tmp is String and tmp.is_valid_int()) or (tmp is int)
         
-    func base_is_number(a = null, _UNUSED_=null):
+    static func base_is_number(a = null, _UNUSED_=null):
         return a is int or a is float
         
     ## Splits the string into paths
-    func string_to_path(str: String):
+    static func string_to_path(str: String):
         var path = []
         for result in string_to_path_rx.search_all(str):
             var key = result.get_string(&"key")
@@ -192,12 +196,12 @@ class Internal:
                     
     
     ## Attempt to get an index from an array or retural the default (null)	
-    func get_index(array:Array, index:int, default_value = null):
+    static func get_index(array:Array, index:int, default_value = null):
         if index >= 0 and index < array.size():
             return array[index]
         return default_value
     
-    func base_get_prop(thing, path, default_value = null):
+    static func base_get_prop(thing, path, default_value = null):
         var splits
         if path is String:
             var result = base_get_prop(thing, [path], null)
@@ -270,8 +274,7 @@ class Internal:
         
     ## For functions with weird and special behaviors when used as n iteratee.
     ## These functions are considered "guarded"
-    ##
-    func get_iteratee(callable: Callable):
+    static func get_iteratee(callable: Callable):
         """
         FAQ Portion:
         Q: Why reference a method by index e.g. 
@@ -297,11 +300,11 @@ class Internal:
     ## 		vs
     ## _.map([[1, 2, 3], [4, 5, 6], [7, 8, 9]], (a,b)=> _.take(a,b))
     ## _.mapValues({a:[1, 2, 3], b:[4, 5, 6], c:[7, 8, 9]},(a,b) => _.take(a,b))
-    func iteratee_drop_second_arg(callable:Callable):
+    static func iteratee_drop_second_arg(callable:Callable):
         return func (arg1, _UNUSED_):
             return callable.call(arg1, null)
             
-    func base_at(obj, arguments: Array):
+    static func base_at(obj, arguments: Array):
         var array = []
         var paths
         for arg in arguments:
@@ -311,7 +314,7 @@ class Internal:
                 array.append(base_get_prop(obj, p))
         return array
         
-    func base_concat(array:Array, arguments:Array):
+    static func base_concat(array:Array, arguments:Array):
         var new_array = []
         new_array.append_array(array)
         for arg in arguments:
@@ -324,11 +327,11 @@ class Internal:
                 new_array.append(arg)
         return new_array
     
-    func matches_property(string:String, v):
+    static func matches_property(string:String, v):
         return func (value, _unused = null):
             return base_get_prop(value,string) == v
         
-    func matches(dict:Dictionary) -> Callable:
+    static func matches(dict:Dictionary) -> Callable:
         return func (value, _unused = null):
             var found = true
             for key in dict:
@@ -336,7 +339,7 @@ class Internal:
                 found = found and dict[key] == prop
             return found
             
-    func property(path):
+    static func property(path):
         var splits
         if path is String:
             splits = path.split(&":")
@@ -349,10 +352,10 @@ class Internal:
         return func (value, _unused = null):
             return base_get_prop(value, path)
             
-    func identity(value, _unused = null): 
+    static func identity(value, _unused = null): 
         return value
         
-    func iteratee(iteratee_val):
+    static func iteratee(iteratee_val):
         if is_same(iteratee_val, _UNDEF_):
             return identity
         match typeof(iteratee_val):
@@ -365,7 +368,7 @@ class Internal:
                 var val = base_get_prop(iteratee_val,["1"])
                 return matches_property(prop,val)
             TYPE_NIL:
-                return identity
+                return GD_.identity
             TYPE_CALLABLE:
                 return get_iteratee(iteratee_val)
                     
@@ -381,8 +384,3 @@ static func _is_not_null_arg(i,_i):
     
 static func not_implemented():  
     assert(false, "Not implemented yet. Do you need this function? If so, open an issue and I will prioritize it")
-
-## Ensures that when it iterates through the item, it always iterates via keys
-## This does not have a lodash equivalent	
-static func keyed_iterable(thing, from_index = 0):
-    return __INTERNAL__.keyed_iterable(thing, from_index)
